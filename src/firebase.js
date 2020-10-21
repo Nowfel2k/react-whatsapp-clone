7080ec2d5a1a2a2a20530a457fd47740bffc3187
Navfal <55524903+Nowfel2k@users.noreply.github.com>
2020-10-21 06:14:47 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDsws02OXM9MdzvWO8MUGugi36PQG_oNGY',
-  authDomain: 'whatsapp-clone-75c3a.firebaseapp.com',
-  databaseURL: 'https://whatsapp-clone-75c3a.firebaseio.com',
-  projectId: 'whatsapp-clone-75c3a',
-  storageBucket: 'whatsapp-clone-75c3a.appspot.com',
-  messagingSenderId: '908683671152',
-  appId: '1:908683671152:web:4aef545b3f5e38d05ee6b5',
-  measurementId: 'G-TDFG9JC8QL'
+  
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
